78bbfc343c522227ff95dca8c7a35cdef6f3777e
Ronaldo Lopes <ronaldofslopes@gmail.com>
2022-05-15 16:52:30 -0300

    alterado texto do componenten de contato

--- src/components/Contato.tsx
+++ src/components/Contato.tsx
@@ -1,16 +1,18 @@
 import instagram from '/public/instagram.svg'
+import whatsappIcon from '/public/whatsapp.svg'
+import Image from 'next/image'
 
 export const Contato = () => (
   <>
-    <div className="flex w-full justify-center">
+    <div className="w-full text-center">
       <h3 className="pt-[7.5rem]">
-        Está procurando um desenvolvedor para ajudar em seu projetsso?
+        Está procurando um desenvolvedor para ajudar em seu projeto?
       </h3>
-      <h2 className="pt-8">Fale comigo!</h2>
+      <h2 className="pt-8">Fale comigo</h2>
       <h1 className="pt-8">ronaldofslopes@gmail.com</h1>
     </div>
     <div>
-      <img src={instagram} alt="teste" />
+      <img src={whatsappIcon} alt="teste" title="icone do whatsapp" />
     </div>
   </>
 )
