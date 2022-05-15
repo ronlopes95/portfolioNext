849be6729fa98baba30e65a883dee38e40e3ee57
Ronaldo Lopes <ronaldofslopes@gmail.com>
2022-05-15 10:54:08 -0300

    Adicionando novo componente de descricao

---
+++ src/components/Description.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image'
+
+export const Description = () => (
+  <div>
+    <div className="flex w-full">
+      <h2 className="font flex w-full justify-center pt-[7.5rem]">
+        Get to know me
+      </h2>
+    </div>
+
+    <div className="flex justify-between gap-[5rem]">
+      <Image
+        src="/photo2.svg"
+        alt="Ilustração de um menino mexendo no computador com vários monitores"
+        width="512"
+        height="512"
+      />
+
+      <p className="max-w-[38rem] flex-1 items-center justify-between pt-[7.563rem]">
+        I implement user interface design and solve user problems with HTML, CSS
+        AND Javascript. I have 2 years of making products that solve user
+        problems and implementing responsive website. I implement user interface
+        design and solve user problems with HTML, CSS AND Javascript. I have 2
+        years of making products that solve user problems and implementing
+        responsive website.I implement user interface design and solve user
+        problems with HTML, CSS AND Javascript. I have 2 years of making
+        products that solve user problems and implementing responsive website.
+        Contact me
+      </p>
+    </div>
+  </div>
+)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { Description } from '../components/Description'
 
 import { Hero } from '../components/Hero'
 import { Navbar } from '../components/Navbar'
@@ -13,6 +14,7 @@ const Home: NextPage = () => {
       </Head>
       <Navbar />
       <Hero />
+      <Description />
     </div>
   )
 }
